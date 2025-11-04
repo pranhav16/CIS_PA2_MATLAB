d23dc5d1b9da9ccd4e1eba92f1a3cbad57feb60b
Pranhav Sundararajan <pranhav16@gmail.com>
2025-11-03 20:58:42 -0500

    Adding script to run the assignment

---
+++ CIS_2025/CISPA_Matlab/programs/pa2_run_script.m
@@ -0,0 +1,13 @@
+%% PA2 Main Script
+% Run this script to attain the output file. Change mode to either debug or
+% unknown and letter_index to the appropriate letter index for either debug
+% or unknown (a-f for debug and g-j for unknown). Keep degree at 5.
+
+mode = 'debug';
+letter_index = 'a';
+degree = 5;
+
+
+
+
+assignment_2(mode,letter_index,degree);
